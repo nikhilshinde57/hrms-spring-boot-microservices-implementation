2b19559e36fd5203a18488afb70a29ea7740fe61
Nikhil Shinde <nikhil.shinde@healthfidelity.com>
2020-05-05 00:08:48 +0530

    Updated script to create leave_service database

--- src/main/resources/sql-scripts/db-setup-script.sql
+++ src/main/resources/sql-scripts/db-setup-script.sql
@@ -21,5 +21,11 @@ CREATE DATABASE employee_service
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
 
+CREATE DATABASE leave_service
+    WITH
+    OWNER = hrms
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
 --Run this below command on above created databases to enable UUID on it.
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
