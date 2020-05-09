95c0df92b6106ec323f46a89e3d2d0e730f4b6ca
Nikhil Shinde <nikhil.shinde@healthfidelity.com>
2020-05-09 18:59:03 +0530

    Added JPAAuditing, SpringBean, Swagger configs

---
+++ leave-service/src/main/java/com/niks/leaveservice/config/SpringBeanConfig.java
@@ -0,0 +1,18 @@
+package com.niks.leaveservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Config class for creation of any ad hoc spring beans
+ */
+@Configuration
+public class SpringBeanConfig {
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+}
