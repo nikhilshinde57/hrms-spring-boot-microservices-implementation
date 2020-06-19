b07d98a53e6f15243d9bd3502a0d143392b7664a
Nikhil Shinde <nikhil.shinde@healthfidelity.com>
2020-06-20 00:10:29 +0530

    JWT token implemenation in OAuth

--- oauth2_authentication_service/src/main/java/com/niks/oauth2authenticationservice/security/JWTOAuth2Config.java
+++ oauth2_authentication_service/src/main/java/com/niks/oauth2authenticationservice/security/JWTOAuth2Config.java
@@ -1,87 +1,67 @@
-//package com.niks.oauth2authenticationservice.security;
-//
-//import java.util.Arrays;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-//import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-//import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-//import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
-//import org.springframework.security.oauth2.provider.token.TokenStore;
-//import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-//
-//
-//@Configuration
-//public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
-//
-////  @Autowired
-////  private ServiceConfig serviceConfig;
-////  @Bean
-////  public TokenStore tokenStore() {
-////    return new JwtTokenStore(jwtAccessTokenConverte());
-////  }
-////  @Bean
-////  @Primary
-////  public DefaultTokenServices tokenServices() {
-////    DefaultTokenServices defaultTokenServices
-////        = new DefaultTokenServices();
-////    defaultTokenServices.setTokenStore(tokenStore());
-////    defaultTokenServices.setSupportRefreshToken(true);
-////    return defaultTokenServices;
-////  }
-////
-////  @Bean
-////  public JwtAccessTokenConverter jwtAccessTokenConverter() {
-////    JwtAccessTokenConverter converter =
-////        new JwtAccessTokenConverter();
-////    converter
-////        .setSigningKey(serviceConfig.getJwtSigningKey());
-////    return converter;
-////  }
-////
-////  @Bean
-////  public TokenEnhancer jwtTokenEnhancer() {
-////    return new JWTTokenEnhancer();
-////  }
-//
-//  @Bean
-//  public TokenStore tokenStore() {
-//    return new InMemoryTokenStore();
-//  }
-//  @Autowired
-//  private AuthenticationManager authenticationManager;
-//  @Autowired
-//  private UserDetailsService userDetailsService;
-//  @Autowired
-//  private TokenStore tokenStore;
-//  @Autowired
-//  private DefaultTokenServices tokenServices;
-//  @Autowired
-//  private JwtAccessTokenConverter jwtAccessTokenConverter;
-//  @Autowired
-//  private TokenEnhancer jwtTokenEnhancer;
-//
-//  @Override
-//  public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-//      throws Exception {
-//    TokenEnhancerChain tokenEnhancerChain =
-//        new TokenEnhancerChain();
-//    tokenEnhancerChain
-//        .setTokenEnhancers(
-//            Arrays.asList(
-//                jwtTokenEnhancer,
-//                jwtAccessTokenConverter));
-//    endpoints
-//        .tokenStore(tokenStore)
-//        .accessTokenConverter(jwtAccessTokenConverter)
-//        .authenticationManager(authenticationManager)
-//        .userDetailsService(userDetailsService);
-//  }
-//}
+package com.niks.oauth2authenticationservice.security;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Autowired
+  private TokenStore tokenStore;
+
+  @Autowired
+  private DefaultTokenServices tokenServices;
+
+  @Autowired
+  private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+  @Autowired
+  private TokenEnhancer jwtTokenEnhancer;
+
+
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+    tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtTokenEnhancer, jwtAccessTokenConverter));
+
+    endpoints.tokenStore(tokenStore)                             //JWT
+        .accessTokenConverter(jwtAccessTokenConverter)       //JWT
+        .tokenEnhancer(tokenEnhancerChain)                   //JWT
+        .authenticationManager(authenticationManager)
+        .userDetailsService(userDetailsService);
+  }
+
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+    clients.inMemory()
+        .withClient("eagleeye")
+        .secret("{noop}thisissecret")
+        .authorizedGrantTypes("refresh_token", "password", "client_credentials")
+        .scopes("webclient", "mobileclient");
+  }
+}
--- oauth2_authentication_service/src/main/java/com/niks/oauth2authenticationservice/security/JWTTokenEnhancer.java
+++ oauth2_authentication_service/src/main/java/com/niks/oauth2authenticationservice/security/JWTTokenEnhancer.java
@@ -1,5 +1,31 @@
 package com.niks.oauth2authenticationservice.security;
 
-public class JWTTokenEnhancer {
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+public class JWTTokenEnhancer implements TokenEnhancer {
+
+//  @Autowired
+//  private OrgUserRepository orgUserRepo;
+//
+//  private String getOrgId(String userName){
+//    UserOrganization orgUser = orgUserRepo.findByUserName( userName );
+//    return orgUser.getOrganizationId();
+//  }
+
+  @Override
+  public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+    Map<String, Object> additionalInfo = new HashMap<>();
+//    String orgId =  getOrgId(authentication.getName());
+
+    additionalInfo.put("NikName", "Nikhil_Shinde");
+
+    ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+    return accessToken;
+  }
 }
